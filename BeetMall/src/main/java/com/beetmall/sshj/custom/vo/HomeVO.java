3bb3d38e09d15cf487f6a66071cdc6442dfe115a
KYS <glowsu14@gmail.com>
2021-05-24 13:07:12 +0900

    중간저장 0524

---
+++ BeetMall/src/main/java/com/beetmall/sshj/custom/vo/HomeVO.java
@@ -0,0 +1,61 @@
+package com.beetmall.sshj.custom.vo;
+
+public class HomeVO {
+	private int productnum;
+	private String thumbimg;
+	private int productprice;
+	private String productname;
+	private String productinfomation;
+	private String catename;
+	private int catesort;
+	private int saleprice;
+	public int getSaleprice() {
+		return saleprice;
+	}
+	public void setSaleprice(int saleprice) {
+		this.saleprice = saleprice;
+	}
+	public int getProductnum() {
+		return productnum;
+	}
+	public void setProductnum(int productnum) {
+		this.productnum = productnum;
+	}
+	public String getThumbimg() {
+		return thumbimg;
+	}
+	public void setThumbimg(String thumbimg) {
+		this.thumbimg = thumbimg;
+	}
+	public int getProductprice() {
+		return productprice;
+	}
+	public void setProductprice(int productprice) {
+		this.productprice = productprice;
+	}
+	public String getProductname() {
+		return productname;
+	}
+	public void setProductname(String productname) {
+		this.productname = productname;
+	}
+	public String getProductinfomation() {
+		return productinfomation;
+	}
+	public void setProductinfomation(String productinfomation) {
+		this.productinfomation = productinfomation;
+	}
+	public String getCatename() {
+		return catename;
+	}
+	public void setCatename(String catename) {
+		this.catename = catename;
+	}
+	public int getCatesort() {
+		return catesort;
+	}
+	public void setCatesort(int catesort) {
+		this.catesort = catesort;
+	}
+	
+}
